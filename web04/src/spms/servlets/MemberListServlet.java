fdb8d7028d767bc0ccf62927f821b6467f034e84
Jaeho Jeong <ywm2004@gmail.com>
2021-02-16 09:55:39 +0900

    [ 실력 향상 문제 ] 2번 완료

--- web04/src/spms/servlets/MemberListServlet.java
+++ web04/src/spms/servlets/MemberListServlet.java
@@ -13,12 +13,15 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/member/list")
-public class MemberListServlet extends GenericServlet {
+public class MemberListServlet extends HttpServlet {
 
 	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
