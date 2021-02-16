84efa92b86ca226b4c3cebb0ef412021895d7b0f
Jaeho Jeong <ywm2004@gmail.com>
2021-02-16 13:16:34 +0900

    [ 필터 ] 완료

--- web04/src/spms/servlets/MemberAddServlet.java
+++ web04/src/spms/servlets/MemberAddServlet.java
@@ -31,7 +31,8 @@ public class MemberAddServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("utf-8");
+		// CharacterEncodingFilter에서 처리
+		//req.setCharacterEncoding("utf-8");
 
 		Connection conn = null;
 		PreparedStatement stmt = null;
